674874cf59a617e672945f6d94eb10497a508763
Aiden Mellor <rdpolarity@gmail.com>
2023-12-09 17:08:16 +1100

    feat: Improved walking animation

--- Assets/Scripts/Bigmode/PlayerController.cs
+++ Assets/Scripts/Bigmode/PlayerController.cs
@@ -7,19 +7,20 @@ namespace Bigmode
     {
         // Configuration
         [SerializeField] private float force;
-        
+        [SerializeField] private float walkingThreshold = 0.2f;
+
         // Working vars
         public Animator walkIdleAnimator;
         private new Rigidbody2D rigidbody;
         private Controls.PlayerActions playerActions;
         private Vector2 movementInput;
-        
+
         // Constants
         private static readonly int IsWalking = Animator.StringToHash("IsWalking");
-        
+
         // Properties
         public Vector3Int Cell => transform.position.Cell();
-        
+
         protected override void Awake()
         {
             rigidbody = GetComponent<Rigidbody2D>();
@@ -41,13 +42,17 @@ namespace Bigmode
         {
             var desired = movementInput * force;
             var delta = desired - rigidbody.velocity;
-            
+
             rigidbody.AddForce(delta);
-            
-            var isMoving = rigidbody.velocity.magnitude > 0.1f;
+
+            var isMoving = rigidbody.velocity.magnitude > walkingThreshold;
             walkIdleAnimator.SetBool(IsWalking, isMoving);
+
+            float speed = rigidbody.velocity.magnitude;
+            if (isMoving) walkIdleAnimator.speed = speed / 2;
+            else walkIdleAnimator.speed = 1;
         }
-        
+
         // IPlayerActions
 
         public void OnMovement(InputAction.CallbackContext context)
