b580b6baa871dbab3bdc8e8faeb8a59bd173b7ed
Aiden Mellor <rdpolarity@gmail.com>
2023-12-12 17:50:16 +1100

    feat: Nav mesh generation

--- Assets/Scripts/Bigmode/StartRuntimeNavmesh.cs
+++ Assets/Scripts/Bigmode/StartRuntimeNavmesh.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using NavMeshPlus.Components;
+using Unity.Entities.UniversalDelegates;
 using UnityEngine;
 
 // This script will update the navmesh at runtime
@@ -7,6 +8,7 @@ public class StartRuntimeNavmesh : MonoBehaviour
 {
     public NavMeshSurface Surface2D;
     public Transform Player;
+    public bool UseUpdateRadius = false;
     private Vector2 lastPosition = Vector2.zero;
     public float UpdateRadius = 5f;
     private bool isBuildingNavMesh = false;
@@ -19,8 +21,13 @@ public class StartRuntimeNavmesh : MonoBehaviour
         Surface2D.BuildNavMeshAsync();
     }
 
+    public void RequestNavMeshUpdate() {
+        UpdateNavMesh();
+    }
+
     void Update()
     {
+        if (!UseUpdateRadius) return;
         // // If the player has moved more than the update radius, update the navmesh
         if (Vector2.Distance(Player.position, lastPosition) > UpdateRadius && !isBuildingNavMesh && Time.time > lastUpdateTime + UpdateCooldown)
         {
