ee412820955a5c16fb163b10d87aa46c1d0d2918
Aiden Mellor <rdpolarity@gmail.com>
2023-12-15 16:48:24 +1100

    chore: Added camera shake to explosions, increased bomber cost to 5

--- Assets/Explosion.cs
+++ Assets/Explosion.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using Bigmode;
+using Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
+using static Cinemachine.CinemachineImpulseDefinition;
 
 public class Explosion : MonoBehaviour
 {
@@ -18,21 +20,28 @@ public class Explosion : MonoBehaviour
 
     void Start()
     {
+        // Apply damage logic
+        ApplyDamage();
 
-        var colliders = Physics2D.OverlapCircleAll(transform.position, radius);
-        if (colliders.Length != 0)
-        {
-            foreach (var collider in colliders)
-            {
-                var entity = collider.GetComponent<Entity>();
-                if (entity != null && collider.CompareTag(damageTag))
-                {
-                    entity.Damage(damage);
-                }
-            }
-        }
+        // Run impulse
+        var impulseSource = GetComponent<CinemachineImpulseSource>();
+        if (impulseSource != null) impulseSource.GenerateImpulse();
 
+        // Clean up
         var ps = GetComponentInChildren<ParticleSystem>();
         Destroy(gameObject, ps.main.duration);
+    }
+
+    private void ApplyDamage()
+    {
+        var colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+        foreach (var collider in colliders)
+        {
+            var entity = collider.GetComponent<Entity>();
+            if (entity != null && collider.CompareTag(damageTag))
+            {
+                entity.Damage(damage);
+            }
+        }
     }
 }
